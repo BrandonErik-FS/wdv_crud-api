937dcbf3c7a2c207622dd50f3e99ea9190e5943a
BrandonErik-FS <EMBrandon@student.fullsail.edu>
2023-10-08 15:36:02 -0400

    Fixed Typo in formatDate Import Statement Changes made in this commit: - In Dashboard.jsx, fixed a typo in the import statement for the formatDate function found in utils/formatDate. This typo was discovered after trying to build the React frontend during the Heroku deployment process.

--- reactjs/src/pages/Dashboard.jsx
+++ reactjs/src/pages/Dashboard.jsx
@@ -1,7 +1,7 @@
 import { useState, useEffect, useRef } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 
-import formatDate from '../utils/FormatDate';
+import formatDate from '../utils/formatDate';
 
 function Dashboard() {
     const [pets, setPets] = useState(null);
