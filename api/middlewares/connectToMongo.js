fc21fd13bf8c7da92b35ef8cc98949f69dffa34d
BrandonErik-FS <EMBrandon@student.fullsail.edu>
2023-11-21 18:37:30 -0500

    Bumped API & React Package Versions Changes made in this commit: - In api/package.json & package-lock.json, upgraded mongoose to 8.0.1. - In reactjs/package.json & package-lock.json, upgraded @vitejs/plugin-react-swc, axios, react-router-dom, styled-components, eslint, @types/react, @types/react-dom, eslint-plugin-react-refresh. - In api/middlewares/connectToMongo.js, removed deprecated useNewUrlParser option.

--- api/middlewares/connectToMongo.js
+++ api/middlewares/connectToMongo.js
@@ -5,7 +5,7 @@ exports.connectToMongoDB = () => {
     const DATABASE_URL =
         process.env.DATABASE_URL || 'mongodb://127.0.0.1:27017/petStoreDB';
 
-    mongoose.connect(DATABASE_URL, { useNewUrlParser: true });
+    mongoose.connect(DATABASE_URL);
     const db = mongoose.connection;
 
     db.on('error', (error) => {
